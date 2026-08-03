6529deb9d7faad408cea859d9b663b0b6367124f
Nchinmayeii <nchinmayeii27@gmail.com>
2026-08-03 16:20:27 +0530

    3

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-08-03T14:45:26+0530",
+    date = "2026-08-03T16:04:12+0530",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
